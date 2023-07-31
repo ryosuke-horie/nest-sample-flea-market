3ba6b5e40ddb8e58c55f72aa09e66f0496f2a2ff
ryosuke-horie <ryosuke03140@gmial.com>
2023-07-31 19:43:33 +0900

    createメソッドの実装

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { Item } from './item.model';
+import { ItemStatus } from './item-status.enum';
 
 @Controller('items')
 export class ItemsController {
@@ -9,5 +11,22 @@ export class ItemsController {
   findAll() {
     // ItemsServiceのfindAll()を呼び出す
     return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string, // @Body()でリクエストを受け取り、idという変数にidを格納する
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+    ): Item {
+      const item: Item = {
+        id, // id: id,と同じ意味（ES6の書き方）
+        name,
+        price,
+        description,
+        status: ItemStatus.ON_SALE,
+      }
+    return this.itemsService.create(item);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
 
 @Injectable() // DI
 export class ItemsService {
+  private items: Item[] = [];
+
   findAll() {
     return 'This is findAll Service';
+  }
+
+  create(item: Item): Item {
+    this.items.push(item);
+    return item;
   }
 }
