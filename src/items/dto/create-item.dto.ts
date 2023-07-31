511366a6ee88b3f42a1596d70d0899659bee35f7
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-01 07:46:07 +0900

    クラスバリデータ

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,18 @@
+import { Type } from "class-transformer";
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from "class-validator";
+
 export class CreateItemDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(40)
     name: string;
+
+    @IsInt()
+    @Min(1)
+    @Type(() => Number)
     price: number;
+
+    @IsString()
+    @IsNotEmpty()
     description: string;
 }
