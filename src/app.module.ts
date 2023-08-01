b453ef44545de2b3127c5c701c4ce45366ffd83b
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-01 19:10:00 +0900

    dbと接続（TypeORM）

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+    })
+  ],
 })
 export class AppModule {}
