210f9e31336d68e18f1d8ede70570fb696586f24
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-03 23:11:09 +0900

    ユーザーエンティティの作成

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { UserStatus } from "src/auth/user-status.enum";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    username: string;
+
+    @Column()
+    password: string;
+    
+    @Column()
+    status: UserStatus;
+}
---
+++ src/migrations/1691071716044-createUser.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createUser1691071716044 implements MigrationInterface {
+    name = 'createUser1691071716044'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "username" character varying NOT NULL, "password" character varying NOT NULL, "status" character varying NOT NULL, CONSTRAINT "UQ_78a916df40e02a9deb1c4b75edb" UNIQUE ("username"), CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
