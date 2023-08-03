2db8eb10c06e92aeb32a46c700547b74000fe181
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-03 23:03:36 +0900

    Authモジュールのセットアップ

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ItemsModule, TypeOrmModule.forRoot()],
+  imports: [ItemsModule, TypeOrmModule.forRoot(), AuthModule],
 })
 export class AppModule {}
