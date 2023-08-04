4da1d9e6ec40c285174a763fde32dca710525891
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-04 12:32:43 +0900

    パスワードのhash化

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,14 +1,20 @@
 import { User } from "src/entities/user.entity";
 import { EntityRepository, Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(createUserDto: CreateUserDto): Promise<User> {
         const { username, password, status } = createUserDto;
+
+        // saltを作成。ハッシュ値の復元をより難しくする
+        const salt = await bcrypt.genSalt();
+        const hashPassword = await bcrypt.hash(password, salt);
+
         const user = this.create({
             username,
-            password,
+            password: hashPassword,
             status
         });
 
