29dc9cd798d2c271c53d6f58259fe0eddb95588e
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-04 14:37:10 +0900

    jwtモジュールの設定

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }), // デフォルトの認証情報をJWTに設定
+    JwtModule.register({ // JWTの設定
+      secret: 'secretKey123', // 秘密鍵。本来は環境変数などに設定して外部にもらさないこと。
+      signOptions: {
+        expiresIn: 3600, // トークンの有効期限(秒)
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
