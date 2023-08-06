92aeac2129206c3c52a4a05bd2f6196b7453f29b
ryosuke-horie <ryosuke03140@gmial.com>
2023-08-07 08:02:13 +0900

    relationを定義

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { UserStatus } from "src/auth/user-status.enum";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Item } from "./item.entity";
 
 @Entity()
 export class User {
@@ -14,4 +15,8 @@ export class User {
     
     @Column()
     status: UserStatus;
+
+    // Itemエンティティと1対多の関係を定義
+    @OneToMany(() => Item, (item) => item.user)
+    items: Item[];
 }
